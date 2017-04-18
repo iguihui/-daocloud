0d673d3f9eb6e04cd0962e4316b93b123ef2b514
WinstonH <bjdhjj@126.com>
2017-04-19 00:48:02 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+var request = require('request');
+var PORT = 8888;
+//重启DaoCloud
+app.get('/http/daocloud/restart/:appid/:token',function(req, res){
+    var appid = req.params.appid;
+    var token = req.params.token;
+    request({
+        method: 'POST',
+        url:"https://openapi.daocloud.io/v1/apps/"+ appid +"/actions/restart",
+        headers: {"Authorization": token}}, 
+        function (error, response, body) {
+            if(error)
+                res.send(error);
+            else
+                res.send(body);
+    });
+ 
+app.listen(PORT);
